f97625d80df84f7482839e676a3c9cd0dd55fe74
Fredrick <fredrick@localhost.localdomain>
2023-07-31 02:46:34 -0400

    added download and delete to files and folders

--- src/components/File.js
+++ src/components/File.js
@@ -4,19 +4,19 @@ import { faFile, faTrash, faDownload } from '@fortawesome/free-solid-svg-icons';
 import { database, storage } from '../firebase';
 import { getStorage, deleteObject, ref, getDownloadURL } from 'firebase/storage';
 import { deleteDoc } from 'firebase/firestore';
+import { saveAs } from 'file-saver';
 
-const File = ({ childFile }) => {  
-  const [showDelete, setShowDelete] = useState(false);
-  const [showDownload, setShowDownload] = useState(false);
+const File = ({ childFile }) => {
+  const [showActions, setShowActions] = useState(true);
 
   const storageRef = getStorage();
-  
+
   const deleteFile = (e) => {
     e.preventDefault();
     e.stopPropagation();
-  
+
     const fileRef = ref(storageRef, childFile.path);
-  
+
     deleteObject(fileRef)
       .then(() => {
         deleteDoc(database.getFile(childFile.id))
@@ -37,52 +37,45 @@ const File = ({ childFile }) => {
     e.stopPropagation();
 
     const fileRef = ref(storageRef, childFile.path);
-    
+
     try {
       const downloadUrl = await getDownloadURL(fileRef);
 
-      // Fetch the file content using the download URL
-      const response = await fetch(downloadUrl);
-      const data = await response.blob();
-
-      // Create a temporary URL to initiate the download
-      const fileURL = URL.createObjectURL(data);
-
-      // Create a dynamic anchor element and click it to initiate download
-      const anchor = document.createElement('a');
-      anchor.href = fileURL;
-      anchor.download = childFile.name;
-      anchor.click();
-
-      // Cleanup the temporary URL
-      URL.revokeObjectURL(fileURL);
+      fetch(downloadUrl, { mode: 'no-cors' })
+        .then((response) => response.blob())
+        .then((blob) => {
+          saveAs(blob, childFile.name);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     } catch (error) {
       console.log(error);
     }
   };
-  
 
   return (
-    <a 
-      onMouseEnter={() => {
-        setShowDelete(true);
-        setShowDownload(true);
-      }} 
-      onMouseLeave={() => {
-        setShowDelete(false);
-        setShowDownload(false);
-      }} 
-      href={childFile.url} 
-      target="_blank" 
-      className='btn btn-outline-dark text-truncate w-100' 
-      rel='noreferrer'
-    >
-      <FontAwesomeIcon icon={faFile} className="me-1" />
-      {childFile.name}
-      {showDelete && <FontAwesomeIcon onClick={deleteFile} className='ms-1' style={{ color: 'red' }} icon={faTrash} />}
-      {showDownload && <FontAwesomeIcon onClick={downloadFile} className='ms-1' icon={faDownload} />}
-    </a>
+    <div>
+      <a
+        href={childFile.url}
+        target="_blank"
+        className='btn btn-outline-dark d-inline-flex align-items-center'
+        rel='noreferrer'
+      >
+        {childFile.type === 'file' && <FontAwesomeIcon icon={faFile} className="me-1" />}
+        {childFile.type === 'music' && <FontAwesomeIcon icon={faFile} className="me-1" />}
+        {childFile.type === 'video' && <FontAwesomeIcon icon={faFile} className="me-1" />}
+        {childFile.name}
+      </a>
+
+      {showActions && (
+        <div className="d-inline-flex">
+          <FontAwesomeIcon onClick={deleteFile} className='ms-1' style={{ color: 'red' }} icon={faTrash} />
+          <FontAwesomeIcon onClick={downloadFile} className='ms-1' icon={faDownload} />
+        </div>
+      )}
+    </div>
   );
-}
+};
 
 export default File;
--- src/components/Folder.js
+++ src/components/Folder.js
@@ -1,15 +1,54 @@
+// import React from 'react';
+// import { Button } from 'react-bootstrap';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faFolder } from '@fortawesome/free-solid-svg-icons';
+// import { Link } from 'react-router-dom';
+
+// const Folder = ({ childFolder }) => {
+//   return (
+//     <Button as={Link} to={`/folder/${childFolder.id}`} state={{ folder: childFolder}} className="text-truncate w-100" variant='outline-dark'>
+//       <FontAwesomeIcon className='me-1' icon={faFolder}></FontAwesomeIcon> {childFolder.name}
+//     </Button>
+//   );
+// }
+
+// export default Folder;
+
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFolder } from '@fortawesome/free-solid-svg-icons';
+import { faFolder, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import { getFirestore, doc, deleteDoc } from 'firebase/firestore';
 
 const Folder = ({ childFolder }) => {
+  const deleteFolder = async (folderId) => {
+    const db = getFirestore();
+    const folderRef = doc(db, 'folders', folderId);
+
+    try {
+      await deleteDoc(folderRef);
+      console.log('Folder deleted successfully.');
+    } catch (error) {
+      console.error('Error deleting folder:', error);
+    }
+  };
+
+  const handleDeleteFolder = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const confirmDelete = window.confirm(`Are you sure you want to delete the folder "${childFolder.name}"?`);
+    if (confirmDelete) {
+      deleteFolder(childFolder.id);
+    }
+  };
+
   return (
-    <Button as={Link} to={`/folder/${childFolder.id}`} state={{ folder: childFolder}} className="text-truncate w-100" variant='outline-dark'>
-      <FontAwesomeIcon className='me-1' icon={faFolder}></FontAwesomeIcon> {childFolder.name}
+    <Button as={Link} to={`/folder/${childFolder.id}`} state={{ folder: childFolder }} className="text-truncate w-100" variant="outline-dark">
+      <FontAwesomeIcon className="me-1" icon={faFolder}></FontAwesomeIcon> {childFolder.name}
+      <FontAwesomeIcon onClick={handleDeleteFolder} className="ms-1" style={{ color: 'green' }} icon={faTrash} />
     </Button>
   );
-}
+};
 
 export default Folder;
